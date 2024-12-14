38d2cf8e2112363a92273882807eccc638a23fd9
Comi <61930403+pcomi@users.noreply.github.com>
2024-12-15 00:07:56 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import sys
+import ipaddress
+import socket
+import threading
+import time
+
+def __main__(argv):
+    if(not argv[1]):
+        print("Incorrect use of command")
+        return 0
+    print("Test")
+
+if __name__ == "__main__":
+    __main__(sys.argv)
